0096b1e17b488966c4b847e7c43b4a7038450049
Naman Singhai <46226154+NamanSinghai@users.noreply.github.com>
2021-02-06 22:26:58 +0530

    Update FlaskApi.py

--- FlaskApi.py
+++ FlaskApi.py
@@ -6,26 +6,14 @@
 ########################################################################################################################################
 
 
-import pandas as pd
-from datetime import datetime as dt
+# import pandas as pd
 from flask import Flask, request
-
-
-data = pd.read_csv("E:\Downloads\FlaskAPI\Monarchs-of-England.csv", names=["s", "e", "m"]).set_index("m")
-
-series = pd.Series(index=range(data.s.min(), dt.now().year))
-
-for m in data.index:
-    series.loc[data.loc[m].s:data.loc[m].e] = m
+# from datetime import datetime as dt
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route("/")
 def home():
-    year = int(request.args["year"])
-    try:
-        return "THIS IS MY HOME PAGE"
-    except Exception as e:
-        print(f"EXCEPTION RAISED: {e}")
-
-
+    return {
+        "message": "THIS IS A FLASK API"
+    }
